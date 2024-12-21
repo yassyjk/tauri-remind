dbb5f99d66dcc57c83437dc7d946bf1b42a54ed2
yassyjk <yassyjk@gmail.com>
2024-12-21 20:53:06 +0900

    android-dev

---
+++ cmdline-tools/set.sh
@@ -0,0 +1,2 @@
+export PATH=${HOME}/Android/SDK/cmdline-tools/latest/bin:${HOME}/Android/SDK/ndk-bundle/:$PATH
+export ANDROID_SDK_ROOT=${HOME}/Android/SDK/
---
+++ remind/src-tauri/gen/android/app/src/main/java/com/remind_app/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.remind_app.app
+
+class MainActivity : TauriActivity()
